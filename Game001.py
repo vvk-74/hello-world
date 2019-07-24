6efb89c6b717a8604b98750268e34b0cdf7d47ab
1 <89298244011@mail.ru>
2019-07-24 16:19:23 +0300

    вывод окна на экран

--- Game001.py
+++ Game001.py
@@ -1,5 +1,22 @@
 import pygame
 pygame.init()
 
-win = pygame.display.set_mode((800,600))
+display_width = 800
+display_heidht = 600
+
+display = pygame.display.set_mode((display_width, display_heidht))
 pygame.display.set_caption('Дино')
+
+def run_game():
+    game = True
+
+    while game:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                quit()
+
+        display.fill((255, 255, 255))
+
+run_game()
+
